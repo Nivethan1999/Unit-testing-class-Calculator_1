cc2c7cd065ad8ea4fcce1b8edcc4c4dcc06413b2
Nivethan1999 <m-iquaz@hotmail.com>
2022-09-01 11:54:51 +0200

    Add project files.

---
+++ Calculator_Test/CalculatorTestUnit.cs
@@ -0,0 +1,47 @@
+using CalcultorClass;
+
+namespace Calculator_Test
+{
+    public class CalculatorTestUnit
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(2, 5, 7)]
+        [TestCase(10, 20, 30)]
+        [TestCase(10, 10, 20)]
+        [TestCase(50, 50, 100)]
+        [TestCase(100, 50, 150)]
+
+        public void AddTest(double p1, double p2, double expected)
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Add(p1, p2), Is.EqualTo(expected));
+
+        }
+
+        [Test]
+        public void MinusTest()
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Subtract(10,5),Is.EqualTo(5));
+        }
+
+        [Test]
+        public void MultiplyTest()
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Multiply(10, 5), Is.EqualTo(50));
+        }
+
+        [Test]
+        public void PowTest()
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Power(2, 2), Is.EqualTo(4));
+        }
+
+    }
+}

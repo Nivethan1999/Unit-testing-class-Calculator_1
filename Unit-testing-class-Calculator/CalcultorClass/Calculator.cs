d002501fa17af65d0df48afaab4d7bbfcf4da986
Joachim Horshauge <jh.hors@gmail.com>
2022-09-05 09:41:11 +0200

    Tests updated (3/5) overloaded functions

--- Unit-testing-class-Calculator/CalcultorClass/Calculator.cs
+++ Unit-testing-class-Calculator/CalcultorClass/Calculator.cs
@@ -1,26 +1,63 @@
-﻿namespace CalcultorClass
+﻿namespace CalculatorClass
 {
     public class Calculator
     {
+        //Variables
+        public double Accumulator { get; private set; }
+        
+        //Addition
         public double Add(double a, double b)
         {
-            return a + b;
+            Accumulator = a + b;
+            return Accumulator;
         }
-
+        
+        //Subtraction
         public double Subtract(double a, double b)
         {
-            return a - b;
+            Accumulator = a - b;
+            return Accumulator;
         }
-
+        
+        //Multiplication
         public double Multiply(double a, double b)
         {
-            return a * b;
+            Accumulator = a * b;
+            return Accumulator;
         }
-
+        
+        public double Multiply(double a)
+        {
+            Accumulator = a * Accumulator;
+            return Accumulator;
+        }
+        
+        //Division
+        public double Divide(double a)
+        {
+            if (a == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            Accumulator = Accumulator / a;
+            return Accumulator;
+        }
+ 
+        //Powers
         public double Power(double x, double exp)
         {
-            return Math.Pow(x, exp);
+            Accumulator = Math.Pow(x, exp);
+            return Accumulator;
         }
+        
+        public double Power(double exp)
+        {
+            Accumulator = Math.Pow(Accumulator, exp);
+            return Accumulator;
+        }
+        
+        
+        
 
     }
 }
